980bd75cb1c8c494c9d8366f48cf7f091aee3313
John Mark Delima <delimajohnmark14@gmail.com>
2023-07-05 18:04:12 +0800

    Improve the log in and register modals

--- world_news/views.py
+++ world_news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.conf import settings
 from datetime import datetime
 from django.http import HttpResponse
+import requests
 
 def index(request):
     # Get the search query parameter
