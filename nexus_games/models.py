9854f339d3b209b5a453b2a5bb4f31a5e1e21b8b
John Mark Delima <delimajohnmark14@gmail.com>
2023-07-26 14:37:09 +0800

    Fix the models

--- nexus_games/models.py
+++ nexus_games/models.py
@@ -3,6 +3,5 @@ from nexus_pub.models import User
 
 class GameResult(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    points = models.IntegerField(default=0)
     wordle_played = models.IntegerField(default=0)
     wordle_won = models.IntegerField(default=0)
--- nexus_games/views.py
+++ nexus_games/views.py
@@ -5,17 +5,22 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 
 from nexus_pub.models import User
+from .models import GameResult
 
 def games(request):
     return render(request, "games/games.html", {"games": True})
 
+@login_required
 def wordle(request):
     user = request.user
-    wordle_data = user
-    games_played = wordle_data
-    games_won = wordle_data
+    game_results = GameResult.objects.filter(user=user)
     
-    percentage_wins = 0
+    # Calculate games_played and games_won for Wordle
+    games_played = sum(result.wordle_played for result in game_results)
+    games_won = sum(result.wordle_won for result in game_results)
+    
+    # Calculate percentage_wins (avoid division by zero)
+    percentage_wins = (games_won * 100) / games_played if games_played > 0 else 0
 
     return render(request, "games/wordle.html", {
         'games_played': games_played,
